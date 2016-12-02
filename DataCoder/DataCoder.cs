dc727c88f360bb8a8eafc4d35ccdc1d6bf571a8b
Vierling, Marvin <marvinvierling@hotmail.de>
2016-12-02 23:06:11 +0100

    init

---
+++ DataCoder/DataCoder.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCoder
+{
+    public class Coder
+    {
+        private MemoryStream stream;
+        public Coder()
+        {
+            stream = new MemoryStream();
+        }
+
+        public void addByte(byte b)
+        {
+            stream.WriteByte((byte)TypCode.BYTE);
+            stream.WriteByte(b);
+        }
+
+        public void addInteger(int i)
+        {
+            stream.WriteByte((byte)TypCode.INTEGER);
+            stream.WriteByte((byte)(i >> 24));
+            stream.WriteByte((byte)(i >> 16));
+            stream.WriteByte((byte)(i >> 8));
+            stream.WriteByte((byte)i);
+        }
+
+        public void addString(string s)
+        {
+            stream.WriteByte((byte)TypCode.STRING);
+            s += "\0";
+            foreach (char item in s.ToCharArray())
+            {
+                stream.WriteByte((byte)(item >> 8));
+                stream.WriteByte((byte)(item));
+            }
+        }
+
+        public void addLong(long l)
+        {
+            stream.WriteByte((byte)TypCode.LONG);
+            stream.WriteByte((byte)(l >> 56));
+            stream.WriteByte((byte)(l >> 48));
+            stream.WriteByte((byte)(l >> 40));
+            stream.WriteByte((byte)(l >> 32));
+            stream.WriteByte((byte)(l >> 24));
+            stream.WriteByte((byte)(l >> 16));
+            stream.WriteByte((byte)(l >> 8));
+            stream.WriteByte((byte)l);
+        }
+
+        public Stream getStream()
+        {
+            return stream;
+        }
+    }
+    public static class Decoder
+    {
+        public static List<object> getData(Stream stream)
+        {
+            List<object> result = new List<object>();
+            long pos = stream.Position;
+            stream.Position = 0;
+            StreamParser(stream, result);
+            stream.Position = pos;
+            return result;
+        }
+
+        private static void StreamParser(Stream stream, List<object> result)
+        {
+            int i = stream.ReadByte();
+            while (i != -1)
+            {
+                byte b = (byte)i;
+                switch (b)
+                {
+                    case (byte)TypCode.BYTE:
+                        result.Add(ReadByte(stream));
+                        break;
+                    case (byte)TypCode.INTEGER:
+                        result.Add(ReadInteger(stream));
+                        break;
+                    case (byte)TypCode.STRING:
+                        result.Add(ReadString(stream));
+                        break;
+                    case (byte)TypCode.LONG:
+                        result.Add(ReadLong(stream));
+                        break;
+                    default:
+                        break;
+                }
+                i = stream.ReadByte();
+            }
+        }
+
+        private static long ReadLong(Stream stream)
+        {
+            long l = 0;
+            for (int x = 1; x <= 8; x++)
+            {
+                long y = stream.ReadByte();
+                l = l | (y << (64 - x * 8));
+            }
+            return l;
+        }
+
+        private static string ReadString(Stream stream)
+        {
+            int iChar = (stream.ReadByte() << 8) | stream.ReadByte();
+            List<char> charList = new List<char>();
+            char c = (char)iChar;
+            while (c != '\0')
+            {
+                charList.Add(c);
+                iChar = (stream.ReadByte() << 8) | stream.ReadByte();
+                c = (char)iChar;
+            }
+            return new string(charList.ToArray());
+        }
+
+        private static int ReadInteger(Stream stream)
+        {
+            int i = 0;
+            for (int x = 1; x <= 4; x++)
+            {
+                int y = stream.ReadByte();
+                i = i | (y << (32 - x * 8));
+            }
+            return i;
+        }
+
+        private static byte ReadByte(Stream stream)
+        {
+            return (byte)stream.ReadByte();
+        }
+    }
+    public enum TypCode
+    {
+        BYTE = 1,
+        INTEGER = 2,
+        STRING = 3,
+        LONG = 4
+    }
+}
---
+++ DataCoderTest/Test.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataCoder;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace DataCoderTest
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestCoderDecoder()
+        {
+            int upperInt = int.MaxValue;
+            int lowerInt = int.MinValue;
+            byte upperByte = byte.MaxValue;
+            byte lowerByte = byte.MinValue;
+            string testString = "ABCabc123.,-/ÜÜ\n";
+            long upperLong = long.MaxValue;
+            long lowerLong = long.MinValue;
+
+            Coder c = new Coder();
+
+            c.addInteger(upperInt);
+            c.addInteger(lowerInt);
+            c.addByte(upperByte);
+            c.addByte(lowerByte);
+            c.addString(testString);
+            c.addLong(upperLong);
+            c.addLong(lowerLong);
+
+            List<object> result = Decoder.getData(c.getStream());
+            Assert.AreEqual(7, result.Count);
+            object[] aResult = result.ToArray();
+            Assert.IsInstanceOfType(aResult[0], typeof(int));
+            Assert.IsInstanceOfType(aResult[1], typeof(int));
+            Assert.IsInstanceOfType(aResult[2], typeof(byte));
+            Assert.IsInstanceOfType(aResult[3], typeof(byte));
+            Assert.IsInstanceOfType(aResult[4], typeof(string));
+            Assert.IsInstanceOfType(aResult[5], typeof(long));
+            Assert.IsInstanceOfType(aResult[6], typeof(long));
+
+            Assert.AreEqual(upperInt, aResult[0]);
+            Assert.AreEqual(lowerInt, aResult[1]);
+            Assert.AreEqual(upperByte, aResult[2]);
+            Assert.AreEqual(lowerByte, aResult[3]);
+            Assert.AreEqual(testString, aResult[4]);
+            Assert.AreEqual(upperLong, aResult[5]);
+            Assert.AreEqual(lowerLong, aResult[6]);
+
+            FileStream fs = new FileStream("test.stream", FileMode.Create);
+            Stream s = c.getStream();
+            long pos = s.Position;
+            s.Position = 0;
+            s.CopyTo(fs);
+            s.Position = pos;
+            fs.Flush();
+            fs.Close();
+        }
+    }
+}
